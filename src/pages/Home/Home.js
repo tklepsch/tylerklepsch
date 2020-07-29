4ac886c478514ded043ab8ecc6a2746682f415a7
Tyler Klepsch <tyler.klepsch@gmail.com>
2020-07-29 11:03:48 -0400

    adding fonts. styling simple homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,10 +3,10 @@ import React from 'react';
 import classes from './Home.module.css';
 
 const Home = () => (
-  <div>
+  <div className={classes.homeContainer}>
     <h1 className={classes.title}>Hi there, I'm Tyler Klepsch</h1>
-    <p className={classes.text}>I am a full-stack web developer with aspirations of self-improvment. For the last few years, I’ve specilized in Drupal development.</p>
-    <p className={classes.text}>Starting with this website, I’m looking to expand off that. I love working with awesome people on fun projects. Want to chat more? Send me an email or <a href="https://www.linkedin.com/in/tyler-klepsch-92004926/">find me on LinkedIn.</a></p>
+    <p className={classes.text}>I am a full-stack web developer with aspirations of self-improvment. For the last few years, I’ve specilized in <span className={classes.highlight}>Drupal development.</span></p>
+    <p className={classes.text}>Starting with this website, I’m looking to expand off that. I love working with awesome people on fun projects. Want to chat more? Send me an email or <a href="https://www.linkedin.com/in/tyler-klepsch-92004926/" className={classes.link}>find me on LinkedIn.</a></p>
   </div>
 )
 
